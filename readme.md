91503563c8cb70c51b4bed79ac762ab496f4137f
50512 <me@50512.dev>
2026-01-06 02:21:25 -0500

    add txt_to_db.py and readme.md

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Validador masivo RUC SUNAT
+
+Este proyecto permite verificar una lista de RUC's, ya sean RUC 10 o RUC 20, directamente con el patrón oficial de la SUNAT, devolviendo: **RUC, razón social, estado del contribuyente y estado domiciliario.**
+
+Todo esto se obtiene del [Padrón reducido RUC](https://www.sunat.gob.pe/descargaPRR/mrc137_padron_reducido.html) que ofrece la propia SUNAT, el cual posteriormente se optimiza y convierte en una base de datos SQL para un acceso eficiente y consultas rápidas.
---
+++ utils/txt_to_db.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import sqlite3
+import time
+from os import path
+
+def connect_db_file(db_path):
+    if not path.exists(db_path):
+        with open(db_path, "w"):
+            print(db_path, "create")
+    
+    connection = sqlite3.connect(db_path)
+    return connection
+
+
+def sanitize_csv(input_file, output_file, expected_fields=4, separator="|"):
+    clean_lines = []
+    
+    # open file on latin-1 for correct reading
+    with open(input_file, "r", encoding="latin-1") as file:
+        for i, line in enumerate(file):
+            line = line.strip()
+            
+            fields = line.split(separator, expected_fields)[:expected_fields]
+            
+            if len(fields) == expected_fields:
+                clean_lines.append("|".join(fields))
+            else:
+                print(f"Error en linea {i}")
+    
+    # export file in utf-8 for most compatibility
+    with open(output_file, "w", encoding="utf-8") as file:
+        file.write("\n".join(clean_lines))
+
+
+def convert_txt_to_sql(input_txt, output_db, separator="|", chunk_size=5000):
+    """Convierte una entrada en txt o csv a una base de datos sql"""
+    
+    connection = connect_db_file(output_db)
+    print("Iniciando conversión...")
+    
+    start_time = time.time()
+    try:
+        chunks = pd.read_csv(
+            input_txt, 
+            sep=separator, 
+            chunksize=chunk_size, 
+            encoding="utf-8", 
+            on_bad_lines="warn",
+            engine="python",
+            quoting=3,
+            )
+        for i, chunk in enumerate(chunks):
+            chunk.columns = chunk.columns.str.strip().str.lower().str.replace(' ', '_')
+            chunk.to_sql("main_table", connection, if_exists="append", index=False)
+            print(f'Chunk {i+1} procesado')
+            
+        print(f'Conversion completada en {round(time.time() - start_time, 2)} segundos.')
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        connection.close()
+
+
+def main():
+    """runs in a main execution"""
+    input_file = input("TO SANITIZE: ")
+    output_db = path.splitext(input_file)[0]
+    TEMP_FILE = ".temp_sanitize.txt"
+    
+    sanitize_csv(input_file, TEMP_FILE)
+    convert_txt_to_sql(TEMP_FILE, output_db)
+    
+
+
+if __name__ == "__main__":
+    main()
