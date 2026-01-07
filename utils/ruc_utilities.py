a5cb52579371d4f5fd42049f90a00db26d025d06
50512 <me@50512.dev>
2026-01-06 20:20:02 -0500

    add utils.ruc_utilities.buscar_rucs and utils.ruc_utilities.limpiar_rucs

---
+++ utils/ruc_utilities.py
@@ -0,0 +1,61 @@
+import sqlite3
+
+
+def buscar_rucs(lista_rucs, path_db, table_name="main_table"):
+    rucs_enteros = [int(ruc) for ruc in limpiar_rucs(lista_rucs)]
+    CHUNK_SQL_SIZE = 900
+    resultados=[]
+    
+    con = sqlite3.connect(path_db)
+    cursor = con.cursor()
+    
+    for i in range(0, len(rucs_enteros), CHUNK_SQL_SIZE):
+        lote = rucs_enteros[i: i+CHUNK_SQL_SIZE]
+        
+        placeholder = ",".join("?"*len(lote))
+        consulta = f"SELECT * FROM {table_name} WHERE ruc IN ({placeholder})"
+        
+        cursor.execute(consulta, lote)
+        resultados.extend(cursor.fetchall())
+    
+    con.close()
+    return resultados
+
+
+def limpiar_rucs(lista_rucs):
+    rucs_limpios = []
+    for doc in lista_rucs:
+        doc = str(doc).strip()
+        ruc_final = ""
+        
+        # Lógica de conversión
+        if len(doc) == 11 and doc.isdigit():
+            digito = digito_verificador_ruc(doc)
+            
+            # corrige el digito de verificación del RUC ingresado de ser necesario
+            if not doc.endswith(str(digito)):
+                ruc_final = doc.removesuffix(doc[10]) + str(digito)
+            else:
+                ruc_final = doc
+        
+        # para encontrar RUC 10 con solo el DNI
+        elif len(doc) == 8 and doc.isdigit():
+            base = "10" + doc
+            digito = digito_verificador_ruc(base)
+            ruc_final = base + str(digito)
+        if ruc_final:
+            rucs_limpios.append(ruc_final)
+
+    return rucs_limpios
+
+
+def digito_verificador_ruc(ruc_base):
+    factores = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
+    suma = sum(int(ruc_base[i]) * factores[i] for i in range(10))
+    residuo = suma % 11
+    diferencia = 11 - residuo
+    return diferencia-10 if diferencia>=10 else diferencia
+
+
+if __name__ == "__main__":
+    print(digito_verificador_ruc(input("Ingrese ruc: ")))
