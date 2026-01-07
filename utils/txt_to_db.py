aa892fdeb785660f44b01abd9d53900d9d3bb29a
50512 <me@50512.dev>
2026-01-06 19:21:20 -0500

    add indexing in utils.txt_to_db.convert_txt_to_sql to improve search queries in db

--- utils/txt_to_db.py
+++ utils/txt_to_db.py
@@ -3,14 +3,6 @@ import sqlite3
 import time
 from os import path, remove
 
-def connect_and_create_db_file(db_path):
-    if not path.exists(db_path):
-        with open(db_path, "w"):
-            print(db_path, "create")
-    
-    connection = sqlite3.connect(db_path)
-    return connection
-
 
 def count_lines(file_path):
     with open(file_path, "rb") as file:
@@ -45,11 +37,17 @@ def sanitize_csv(input_file, output_file, expected_fields=4, separator="|"):
         file.write("\n".join(clean_lines))
 
 
-def convert_txt_to_sql(input_txt, output_db, separator="|", chunk_size=5000, progress_callback=None):
+def convert_txt_to_sql(input_txt, output_db, table_name="main_table", separator="|", chunk_size=5000, progress_callback=None):
     """Convierte una entrada en txt o csv a una base de datos sql"""
     
-    connection = connect_and_create_db_file(output_db)
+    connection = sqlite3.connect(output_db)
+    cursor = connection.cursor()
     print("Iniciando conversión...")
+    
+    # Deshabilita seguridad para aumentar velocidad (solo en la conversión)
+    cursor.execute("PRAGMA synchronous = OFF")
+    cursor.execute("PRAGMA journal_mode = MEMORY")
+    cursor.close()
     
     start_time = time.time()
     rows_processed = 0
@@ -61,12 +59,20 @@ def convert_txt_to_sql(input_txt, output_db, separator="|", chunk_size=5000, pro
             chunksize=chunk_size, 
             encoding="utf-8", 
             on_bad_lines="warn",
-            engine="python",
+            engine="c",
+            low_memory=False,
             quoting=3,
+            dtype="str"
             )
+        
         for i, chunk in enumerate(chunks):
             chunk.columns = chunk.columns.str.strip().str.lower().str.replace(' ', '_')
-            chunk.to_sql("main_table", connection, if_exists="append", index=False)
+            
+            if 'ruc' in chunk.columns:
+                # 'coerce' convierte cualquier error a NaN
+                chunk['ruc'] = pd.to_numeric(chunk['ruc'], errors='coerce').fillna(0).astype('int64')
+            
+            chunk.to_sql(table_name, connection, if_exists="append", index=False)
             
             rows_in_chunk = len(chunk)
             rows_processed += rows_in_chunk
@@ -75,12 +81,25 @@ def convert_txt_to_sql(input_txt, output_db, separator="|", chunk_size=5000, pro
                 progress_callback(min(rows_processed/total_lines, 1.0))
             else:
                 print(f'Chunk {i+1} procesado')
-            
-            
-        print(f'Conversion completada en {round(time.time() - start_time, 2)} segundos.')
+        
+        print("Indexando la tabla")
+        index_time = time.time()
+        
+        cursor_idx = connection.cursor()
+        cursor_idx.execute(f"CREATE UNIQUE INDEX IF NOT EXISTS idx_ruc ON {table_name}(ruc)")
+        cursor_idx.close()
+        
+        connection.commit()
+        print(f"Índice creado en {round(time.time() - index_time, 2)}s")
+        print(f'Conversion completada en {round(time.time() - start_time, 2)}s')
     except Exception as e:
+        connection.close()
+        remove(output_db)
         print(f"Error: {e}")
     finally:
+        cursor = connection.cursor()
+        cursor.execute("PRAGMA synchronous = NORMAL")
+        cursor.execute("PRAGMA journal_mode = DELETE")
         connection.close()
 
 
@@ -89,10 +108,11 @@ def main():
     input_file = input("TO SANITIZE: ")
     output_db = path.splitext(input_file)[0]+".db"
     TEMP_FILE = ".temp_sanitize.txt"
-    
+
     sanitize_csv(input_file, TEMP_FILE)
     convert_txt_to_sql(TEMP_FILE, output_db)
     remove(TEMP_FILE)
+    # convert_txt_to_sql(input_file, output_db, "padron", chunk_size=50000)
 
 
 if __name__ == "__main__":
