c7e6e82c0621f595b15ff6f7a1fca6bad55d2db9
50512 <me@50512.dev>
2026-01-06 23:50:55 -0500

    modify utils.ruc_utilites.buscar_rucs to return results with the input documents

--- utils/ruc_utilities.py
+++ utils/ruc_utilities.py
@@ -2,25 +2,60 @@ import sqlite3
 
 
 def buscar_rucs(lista_rucs, path_db, table_name="main_table"):
-    """"""
+    """
+    Recibe una lista de rucs:str, verifica si son validos y los busca en la base de datos.
+    Devuelve la lista original completa, seguida de la versión limpia de cada ruc, si es que hubiera, y la información extraída de la DB
+    """
     rucs_enteros = [int(ruc) for ruc in limpiar_rucs(lista_rucs)]
     CHUNK_SQL_SIZE = 900
-    resultados=[]
+    
+    db_cache = {}
     
     con = sqlite3.connect(path_db)
     cursor = con.cursor()
+    num_columnas = 0
+    try:
+        for i in range(0, len(rucs_enteros), CHUNK_SQL_SIZE):
+            lote = rucs_enteros[i: i+CHUNK_SQL_SIZE]
+            
+            placeholder = ",".join("?"*len(lote))
+            consulta = f"SELECT * FROM {table_name} WHERE ruc IN ({placeholder})"
+            
+            cursor.execute(consulta, lote)
+            filas = cursor.fetchall()
+            
+            if num_columnas==0 and cursor.description:
+                num_columnas = len(cursor.description)
+            
+            for fila in filas:
+                db_cache[fila[0]] = fila
     
-    for i in range(0, len(rucs_enteros), CHUNK_SQL_SIZE):
-        lote = rucs_enteros[i: i+CHUNK_SQL_SIZE]
-        
-        placeholder = ",".join("?"*len(lote))
-        consulta = f"SELECT * FROM {table_name} WHERE ruc IN ({placeholder})"
-        
-        cursor.execute(consulta, lote)
-        resultados.extend(cursor.fetchall())
+    except Exception as e:
+        print(f"Error consulting DB: {e}")
+        return []
     
-    con.close()
-    return resultados
+    finally:
+        con.close()
+    
+    resultados_finales = []
+    
+    for ruc_listado in lista_rucs:
+        ruc_limpio = limpiar_ruc(ruc_listado)
+        ruc_failed = None
+        
+        if not ruc_limpio:
+            ruc_failed = "RUC INVÁLIDO"
+        elif int(ruc_limpio) not in db_cache:
+            ruc_failed = "NO SE ENCONTRÓ"
+        else:
+            tupla_final = (ruc_listado,) + db_cache[int(ruc_limpio)]
+            resultados_finales.append(tupla_final)
+        
+        if ruc_failed:
+            tupla_vacia = (ruc_listado, limpiar_ruc(ruc_limpio),) + (ruc_failed,) + ("-",) * (num_columnas-2)
+            resultados_finales.append(tupla_vacia)
+    
+    return resultados_finales
 
 
 def limpiar_rucs(lista_rucs):
