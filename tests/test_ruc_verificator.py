1473f4759e77511c1a613cf9bdd717753e9622e9
50512 <me@50512.dev>
2026-01-10 02:24:29 -0500

    improved tests

--- tests/test_ruc_verificator.py
+++ tests/test_ruc_verificator.py
@@ -3,9 +3,28 @@ import pytest
 from massruc.ruc_utils import limpiar_ruc
 
 
-def test_ruc_valido_persona_natural():
-    ruc_prueba = "10123456781"  # ruc estructuralmente válido
+@pytest.mark.parametrize(
+    "entrada, esperado",
+    [
+        ("10123456781", "10123456781"),  # Caso base
+        (" 10123456781 ", "10123456781"),  # Con espacios
+        ("10123456780", "10123456781"),  # Corregir digito verificador
+        (10123456781, "10123456781"),  # Input número
+    ],
+)
+def test_limpiar_ruc_exitoso(entrada, esperado):
+    assert limpiar_ruc(entrada) == esperado
 
-    res = True if limpiar_ruc(ruc_prueba) == ruc_prueba else False
 
-    assert res is True
+@pytest.mark.parametrize(
+    "entrada_invalida",
+    [
+        ("1234567"),  # Corto
+        ("1012345678i"),  # Letras
+        ("10-12345678-1"),  # Guiones
+        (""),  # Vacío
+        (None),  # Nulo
+    ],
+)
+def test_limpiar_ruc_fallido(entrada_invalida):
+    assert limpiar_ruc(entrada_invalida) is None
